c85c01ccad4e700bb3166b119eca60e7574e45d9
tsepanx <77626319+tsepanx@users.noreply.github.com>
2023-04-08 09:24:18 +0300

    omit `exact_match_table`, and write all entries to table, matching by regex

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-import re
 import socket
-
 from pprint import pprint
-from dnslib import DNSRecord, DNSQuestion, RR, QTYPE, A
 
-from utils import print_log, REDIRECT_DNS_IP, HOST_IP, HOST_PORT, MSS, RESPONSE_TTL
+from dnslib import (
+    QTYPE,
+    RR,
+    A,
+    DNSQuestion,
+    DNSRecord,
+)
 
+from utils import (
+    HOST_IP,
+    HOST_PORT,
+    MSS,
+    REDIRECT_DNS_IP,
+    RESPONSE_TTL,
+    build_match_table,
+    match_by_any_regex,
+    print_log,
+)
 
-def match_by_any_regex(regex_dict: dict[str, str], match_qname: str):
-    for (i_regex, i_ip) in regex_dict.items():
-        regex_str_i = f"^{i_regex}$" \
-            .replace(".", r"\.") \
-            .replace("*", ".*")
-
-        if re.compile(regex_str_i).match(match_qname):
-            return i_regex, i_ip
-
-    return None, None
+match_table: dict[str, str] = dict()
 
 
 def handle_dns_request(data: bytes, _: str):
@@ -25,18 +29,15 @@ def handle_dns_request(data: bytes, _: str):
     dns_question: DNSQuestion = dns_resp.questions[0]
     qname = str(dns_question.qname)[:-1]  # Remove "." in the end
 
-    is_redirected = False
-    matched_regex, matched_ip = match_by_any_regex(regex_match_table, qname)
+    matched_regex, matched_ip = match_by_any_regex(match_table, qname)
 
-    if matched_ip or qname in exact_match_table:
-        matched_ip_addr = matched_ip or exact_match_table[qname]
-
+    if matched_ip:
         reply = dns_resp.reply()
-
-        reply.add_answer(RR(qname, QTYPE.A, rdata=A(matched_ip_addr), ttl=RESPONSE_TTL))
+        reply.add_answer(RR(qname, QTYPE.A, rdata=A(matched_ip), ttl=RESPONSE_TTL))
 
         response_data = reply.pack()
         dns_resp = reply
+        is_redirected = False
     else:
         # Redirect to default DNS server
         dns_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -44,7 +45,6 @@ def handle_dns_request(data: bytes, _: str):
 
         response_data, _ = dns_sock.recvfrom(MSS)
         dns_resp = DNSRecord.parse(response_data)
-
         is_redirected = True
 
     print_log(dns_resp, qname, matched_regex, is_redirected)
@@ -65,22 +65,9 @@ def server_main():
 
 
 if __name__ == "__main__":
-    exact_match_table: dict[str, str] = dict()
-    regex_match_table: dict[str, str] = dict()
-
     with open("./custom_hosts", "r") as fin:
-        lines = filter(lambda x: not x.startswith("#"), fin.readlines())
-        lines = map(str.strip, lines)
-        lines = filter(lambda x: x != "", lines)
-        entries_list = map(str.split, list(lines))
+        match_table = build_match_table(fin.readlines())
 
-        for (ip, match_str) in entries_list:
-            if any(s in match_str for s in ["*", "?", "("]):
-                regex_match_table[match_str] = ip
-            else:
-                exact_match_table[match_str] = ip
-
-    pprint(exact_match_table, width=1)
-    pprint(regex_match_table, width=1)
+    pprint(match_table, width=1)
 
     server_main()
--- utils.py
+++ utils.py
@@ -1,21 +1,29 @@
-from dnslib import DNSRecord, RR, QTYPE, DNSQuestion
+import re
 
+from dnslib import (
+    QTYPE,
+    RR,
+    DNSQuestion,
+    DNSRecord,
+)
 
 REDIRECT_DNS_IP = "192.168.1.1"
-HOST_IP = '127.0.0.1'
+HOST_IP = "127.0.0.1"
 HOST_PORT = 53
 MSS = 1024
 RESPONSE_TTL = 60
 
 
-def print_log(dns_record: DNSRecord, qname: str, matched_regex: str | None, is_redirected: bool = False):
+def print_log(
+    dns_record: DNSRecord, qname: str, matched_regex: str | None, is_redirected: bool = False
+):
     def resources_types_to_str(resources: list[RR]) -> str:
         map_f = lambda x: QTYPE[x.qtype if isinstance(x, DNSQuestion) else x.rtype]
         return ",".join((map(map_f, resources)))
 
     def limit_str(s: str, maxlen: int) -> str:
         if len(s) > maxlen:
-            return s[:maxlen - 2] + ".."
+            return s[: maxlen - 2] + ".."
         return s
 
     qtypes_str = resources_types_to_str(dns_record.questions)
@@ -38,9 +46,53 @@ def print_log(dns_record: DNSRecord, qname: str, matched_regex: str | None, is_r
     atypes_maxlen = 15
     atypes_str = limit_str(atypes_str, atypes_maxlen)
 
-    print(f"{qtypes_str:<5} {qname:<{qname_maxlen}} = {ip_addr_str:<{ip_addr_maxlen}} | ({qtypes_str:<5} -> {atypes_str:<{atypes_maxlen}})", end="")
+    print(
+        f"{qtypes_str:<5} "
+        f"{qname:<{qname_maxlen}} = "
+        f"{ip_addr_str:<{ip_addr_maxlen}} | "
+        f"({qtypes_str:<5} -> {atypes_str:<{atypes_maxlen}})",
+        end="",
+    )
 
     if is_redirected:
         print(f" | -> {REDIRECT_DNS_IP}")
     else:
         print()
+
+
+def match_by_any_regex(regex_dict: dict[str, str], match_by: str):
+    """
+    If matched any of key (regex) in dict, return corresponding value
+    :param regex_dict: f.e.:
+        {
+            r"(www\.)?google\.c*": "216.239.38.120",
+            "a.*\.com": "0.0.0.0"
+        }
+    :param match_by:
+        match string, by which to match every regex
+    :return: dict's item (key, value) if matched any, otherwise (None, None)
+    """
+    for i_regex, i_res in regex_dict.items():
+        if re.compile(i_regex).match(match_by):
+            return i_regex, i_res
+
+    return None, None
+
+
+def build_match_table(file_lines: list[str]) -> dict[str, str]:
+    match_table = dict()
+
+    lines = filter(lambda x: not x.startswith("#"), file_lines)
+    lines = map(str.strip, lines)
+    lines = filter(lambda x: x != "", lines)
+
+    entries_list = map(str.split, list(lines))
+
+    for match_res_i, match_regex_i in entries_list:
+        match_regex_i = f"^{match_regex_i}$"
+        match_regex_i = match_regex_i.replace(".", r"\.")
+        match_regex_i = match_regex_i.replace("*", ".*")
+
+        match_table[match_regex_i] = match_res_i
+
+    return match_table
